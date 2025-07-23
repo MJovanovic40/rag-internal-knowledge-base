270731b0760e25d245e78b6df01268a72b791e7d
Milan Jovanovic <milanj1705@gmail.com>
2025-07-23 20:55:54 +0200

    [Backend] Add Mapper

---
+++ api/src/main/java/rs/raf/mjovanovic40/rag_internal_knowledge_base/config/mapper/MapperConfig.java
@@ -0,0 +1,14 @@
+package rs.raf.mjovanovic40.rag_internal_knowledge_base.config.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
