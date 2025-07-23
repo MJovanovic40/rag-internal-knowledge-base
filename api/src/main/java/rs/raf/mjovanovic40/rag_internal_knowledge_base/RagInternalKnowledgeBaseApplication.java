1767626bbbb6fbfc7c09e48b02b431d3a5a30c8e
Milan Jovanovic <milanj1705@gmail.com>
2025-07-23 20:52:04 +0200

    Initial Commit

---
+++ api/src/main/java/rs/raf/mjovanovic40/rag_internal_knowledge_base/RagInternalKnowledgeBaseApplication.java
@@ -0,0 +1,13 @@
+package rs.raf.mjovanovic40.rag_internal_knowledge_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagInternalKnowledgeBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagInternalKnowledgeBaseApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/rs/raf/mjovanovic40/rag_internal_knowledge_base/RagInternalKnowledgeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.raf.mjovanovic40.rag_internal_knowledge_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagInternalKnowledgeBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
